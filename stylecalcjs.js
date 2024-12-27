9f4644be629c8edbdb4c6d3d0130c2d3aa40ce5a
joaopedroso2015 <joaobatistapedroso.pedroso35@gmail.com>
2024-12-27 15:52:38 -0300

    Update stylecalcjs.js

--- stylecalcjs.js
+++ stylecalcjs.js
@@ -1,5 +1,12 @@
 function calcularArea() {
-  const raio = document.getElementById('raio').value;
+  const raioInput = document.getElementById('raio');
+  const raio = parseFloat(raioInput.value);
+
+  if (isNaN(raio)) {
+    alert('Por favor, insira um valor numérico válido para o raio.');
+    return;
+  }
+
   const pi = 3.14159;
   const area = pi * raio * raio;
   document.getElementById('resultado').textContent = `A área da circunferência é: ${area.toFixed(2)} unidades quadradas.`;
